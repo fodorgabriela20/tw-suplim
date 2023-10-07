5bdd95d69de42bb38e0d7965936151d00791a79b
Niculae Stefan Alexandru <niculaestefan18@stud.ase.ro>
2023-10-07 14:48:18 +0300

    func

--- function.js
+++ function.js
@@ -1,10 +1,30 @@
 
 
-const elements = ['B', 'i', 'a', 'n', 'c', 'a'];
+// const elements = ['B', 'i', 'a', 'n', 'c', 'a'];
 
 
-function returnConcat(elements) {
-    console.log(elements.join(''));
+// function returnConcat(elements) {
+//     console.log(elements.join(''));
+// }
+
+//returnConcat(elements);
+
+var a = "casa";
+var b = "pana";
+
+function returnNoCh(a, b) {
+
+    var no = 0;
+    if(a.length != b.length) {
+        return -1;
+    }
+    for(i = 0; i < a.length; i++) {
+        if(a[i] != b[i]){
+            no++;
+        }
+    }
+
+    return no;
 }
 
-returnConcat(elements);
+console.log(returnNoCh(a, b));
